622b040d5c3229c597d40952dbdf73cb5f34292e
Dennis <takeuaway@gmail.com>
2014-10-06 11:00:45 +0800

    Initial Commit

---
+++ AttendenceReport/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  AttendenceReport
+//
+//  Created by Dennis on 6/10/14.
+//  Copyright (c) 2014 Practical-Limits. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
